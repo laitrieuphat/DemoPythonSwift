f78a5ba8bb9fa4c7cae7c5f5b2a766fb25e3ee62
minhl <laiminh1110@gmail.com>
2025-11-01 22:45:10 +0700

    create CameraTestViewController

---
+++ Pods/Target Support Files/Pods-DemoPythonSwift-DemoPythonSwiftUITests/Pods-DemoPythonSwift-DemoPythonSwiftUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DemoPythonSwift_DemoPythonSwiftUITests : NSObject
+@end
+@implementation PodsDummy_Pods_DemoPythonSwift_DemoPythonSwiftUITests
+@end
---
+++ Pods/Target Support Files/Pods-DemoPythonSwiftTests/Pods-DemoPythonSwiftTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DemoPythonSwiftTests : NSObject
+@end
+@implementation PodsDummy_Pods_DemoPythonSwiftTests
+@end
